88dfcdba9df3a52a509e41ecef8a9c53883f3160
Yuri Marinho <ymarinho2025@gmail.com>
2026-04-06 21:31:24 -0300

    Alteração no DB

--- src/Controllers/db.php
+++ src/Controllers/db.php
@@ -1,12 +1,15 @@
 <?php
 
-$host = 'localhost';
-$port = 3306;
-$db   = 'edson';
-$user = 'root';
-$pass = 'wasd';
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$port = (int)($_ENV['DB_PORT'] ?? 3306);
+$db   = $_ENV['DB_NAME'] ?? 'edson';
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? 'wasd';
 
 $mysqli = mysqli_connect($host, $user, $pass, $db, $port);
 
+if (!$mysqli) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+
 return $mysqli;
-?>
